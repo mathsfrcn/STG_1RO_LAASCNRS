924e580e3a731da9640677ce867193e928335f7c
mathisfrcn <mathis.frcn64@gmail.com>
2026-07-02 14:25:46 +0200

    incompréhension

--- STG/PROJET/bae/results/analyse_benders_resultsv2.py
+++ STG/PROJET/bae/results/analyse_benders_resultsv2.py
@@ -52,8 +52,8 @@ def analyze_benders_results(file_path):
     ax.set_xlabel("Gamma")
     ax.set_ylabel(f"Ratio of Times ({ref_method} / {alt_method})")
     
-    ax.fill_between(pivot_df['Gamma'].unique(), 1.0, pivot_df['Speedup'].max() * 1.1, 
-                    color='green', alpha=0.05, label=f'Victory zone {alt_method}')
+    #ax.fill_between(pivot_df['Gamma'].unique(), 1.0, pivot_df['Speedup'].max() * 1.1, color='green', alpha=0.05, label=f'Victory zone {alt_method}')
+    ax.fill_between(pivot_df['Gamma'].unique(), 1.0, pivot_df['Speedup'].mean(), color='green', alpha=0.05, label=f'Victory zone {alt_method}')
     
     ax.legend(title='Tau')
 
